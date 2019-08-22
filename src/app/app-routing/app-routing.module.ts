a5b6a5376adae276109d91d87cfbe0d510783e8f
gangani bhumi <ganganibhumika@gmail.com>
2019-08-22 08:01:08 +0530

    home compoent,header and footer is ready for designe

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+// import { HomeComponent } from '../component/home/home.component';
+
+// const routes: Routes = [
+//   { path: '', pathMatch: 'full', redirectTo: 'home' },
+//   { path: 'home', component: HomeComponent },
+// ]
+@NgModule({
+  imports: [
+    // HomeComponent,
+    // RouterModule.forRoot(routes)
+  ],
+  // exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule }    from '@angular/forms';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+
+
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
+   
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule,
+    RouterModule.forRoot(routes)
+
+   
   ],
-  providers: [],
+  
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy },],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
